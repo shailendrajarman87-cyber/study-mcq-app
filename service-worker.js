ede904696a91febd6e0394bd2d6ff0a75680e431
shailendrajarman87-cyber <shailendrajarman87@gmail.com>
2026-09-05 20:31:04 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "study-mcq-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
